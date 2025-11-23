03bec80dbbd5efc62d72fa5d595a60e1cd76a666
AlexLeiba <Alexleiba13@gmail.com>
2025-11-23 16:13:50 +0200

    prettier and eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,13 @@
 import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+
+import nextVitals from "eslint-config-next/core-web-vitals"; //eslint preset provided by next.js
+// Core web vitals: Google’s key performance metrics for ranking websites.
+// It helps performance,accesibility,seo by: Encourages optimized images
+// Avoids synchronous scripts
+// Prevents slow font-loading patterns
+
+import nextTs from "eslint-config-next/typescript"; //is an ESLint configuration preset built into Next.js that provides TypeScript-specific linting rules for Next.js projects.
+// comes with rules as: correct ts usage,detection typing errors,consistent ts coding standards.
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -13,6 +20,11 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    rules: {
+      // "@typescript-eslint/no-unused-vars": "off",
+    },
+  },
 ]);
 
 export default eslintConfig;
